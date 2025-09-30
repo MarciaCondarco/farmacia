8a22c24ec9bb8ea08f316a64c1f1274273a21c8e
Marcy_ <marciagisseli21@gmail.com>
2025-09-29 23:28:38 -0300

    :sparkles: colocando construtor e metodos get e set no objeto medicamento

--- src/farmacia/model/medicamento.java
+++ src/farmacia/model/medicamento.java
@@ -2,4 +2,29 @@ package farmacia.model;
 
 public class medicamento {
 
+	private String nome;
+	private float valor;
+	
+	//construtor
+	public medicamento(String nome, float valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
 }
